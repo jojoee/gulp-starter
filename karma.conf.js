d6ff09c92ed6f90ab917465ca04a2c6354f80c94
Nathachai Thongniran <inid3a@gmail.com>
2016-01-02 23:44:06 +0700

    Add another test framework

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,34 @@
 module.exports = function(config){
   config.set({
-    files         : [
+    files: [
+      'src/components/lodash/lodash.min.js',
       'src/js/*.js'
     ],
-    basePath      : './',
-    autoWatch     : true,
-    frameworks    : ['jasmine'],
-    browsers      : ['PhantomJS'],
-    plugins       : [
+    basePath: './',
+    autoWatch: true,
+    reporters: ['progress', 'junit'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    captureTimeout: 60000,
+    frameworks: ['mocha', 'sinon-chai'],
+    // frameworks: ['jasmine'],
+    browsers: ['PhantomJS'],
+    // browsers: ['Chrome'],
+    plugins: [
       'karma-chrome-launcher',
       'karma-phantomjs-launcher',
-      'karma-jasmine'
-    ]
+      'karma-mocha',
+      'karma-sinon-chai'
+      // 'karma-jasmine'
+    ],
+    junitReporter: {
+      outputDir: 'test',
+      outputFile: 'unit.xml',
+      suite: '',
+      useBrowserName: true
+    },
+    singleRun: false
   });
 };
--- src/js/script2.spec.js
+++ src/js/script2.spec.js
@@ -1,3 +1,5 @@
+/* jasmine test */
+/*
 describe('A suite', function() {
   it('contains spec with an expectation', function() {
     expect(true).toBe.true;
@@ -6,5 +8,18 @@ describe('A suite', function() {
   it('should be equal', function() {
     expect(add2(2, 4)).toEqual(8);
     expect(add3(2, 4)).toEqual(9);
+  });
+});
+*/
+
+/* mocha, sinon, chai */
+describe('A suite', function() {
+  it('contains spec with an expectation', function() {
+    expect(true).to.be.true;
+  });
+
+  it('should be equal', function() {
+    expect(add2(2, 4)).to.equal(8);
+    expect(add3(2, 4)).to.equal(9);
   });
 });
